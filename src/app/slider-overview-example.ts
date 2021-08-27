ea0341f942c8a0cda549ff1f155538715e125868
charleaux <ctorry@gmail.com>
2021-08-27 10:34:21 -0400

    3 files modified

--- src/app/slider-overview-example.ts
+++ src/app/slider-overview-example.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * @title Basic slider
@@ -6,10 +6,14 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'slider-overview-example',
   templateUrl: 'slider-overview-example.html',
-  styleUrls: ['slider-overview-example.css'],
+  styleUrls: ['slider-overview-example.css']
 })
-export class SliderOverviewExample {}
-
+export class SliderOverviewExample {
+  isVisible = true;
+  minValue = 0;
+  maxValue = 100;
+  stepValue = 5;
+}
 
 /**  Copyright 2021 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
