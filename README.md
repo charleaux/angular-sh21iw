2f7ca53fdf7ae1d6094b386026582c23196f0622
charleaux <ctorry@gmail.com>
2021-08-27 10:29:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sh21iw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sh21iw)
---
+++ src/app/slider-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic slider
+ */
+@Component({
+  selector: 'slider-overview-example',
+  templateUrl: 'slider-overview-example.html',
+  styleUrls: ['slider-overview-example.css'],
+})
+export class SliderOverviewExample {}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
